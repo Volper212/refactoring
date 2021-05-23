aad75efdc72c2878469c471c11856fde7dddd477
Volper212 <volpergaming@gmail.com>
2021-05-23 19:27:53 +0200

    Format

--- connect.php
+++ connect.php
@@ -1,13 +1,9 @@
 <?php
-try
-{
-    if
-    (
-        $sql = new PDO('mysql:host=localhost;dbname=baza;encoding=utf8;port=3306', 'root', '' )
+try {
+    if (
+        $sql = new PDO('mysql:host=localhost;dbname=baza;encoding=utf8;port=3306', 'root', '')
     )
-    echo '';
-}
-catch( PDOException $e )
-{
-    die( 'Nie połączono z bazą "baza"' );
+        echo '';
+} catch (PDOException $e) {
+    die('Nie połączono z bazą "baza"');
 }
