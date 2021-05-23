9d9ef2b7f1894ae6b77665267691811e245eb581
Volper212 <volpergaming@gmail.com>
2021-05-23 19:27:53 +0200

    Change to let and const

--- baza.php
+++ baza.php
@@ -13,17 +13,17 @@ $json = json_encode($a->fetchAll(PDO::FETCH_ASSOC));
 <div id="imie"></div>
 
 <script>
-    j = 0;
-    one = 1;
-    idk = 1;
-    var stuff = <?php print json_encode($json); ?>;
-    var arr = new Array();
+    let j = 0;
+    let one = 1;
+    let idk = 1;
+    const stuff = <?php print json_encode($json); ?>;
+    let arr = new Array();
     arr = JSON.parse(stuff);
 
     console.log(arr[0].pass);
 
-    var html = '<table border><tbody>';
-    for (var i = 0; i < arr.length; i++) {
+    let html = '<table border><tbody>';
+    for (let i = 0; i < arr.length; i++) {
         html += '<tr id="' + arr[j].id + '"><td class="numW">' + (j + 1) + '</td><td><input value="' + arr[j].id + '" disabled></td>' + '<td><input id="id' + one + '" value="' + arr[j].login + '" name="' + idk + '" data-action="edit"></td>' + '<td><input id="id' + (one + 1) + '" value="' + arr[j].pass + '" name="' + (idk + 1) + '" data-action="edit"></td><td><button data-action="dup">duplikuj</button></td><td><button data-action="del">x</button></td><td><button data-action="clear">wyczyść</button></td></tr>';
         j++;
         one += 2;
@@ -40,11 +40,11 @@ $json = json_encode($a->fetchAll(PDO::FETCH_ASSOC));
 
     function klik(e) {
         console.log(e.srcElement);
-        getName = e.srcElement.getAttribute('name');
-        getVal = e.srcElement.value;
-        getId = e.srcElement.parentElement.parentElement.getAttribute('id');
+        const getName = e.srcElement.getAttribute('name');
+        const getVal = e.srcElement.value;
+        const getId = e.srcElement.parentElement.parentElement.getAttribute('id');
         console.log(getId);
-        getAct = e.srcElement.getAttribute('data-action');
+        const getAct = e.srcElement.getAttribute('data-action');
 
         switch (getAct) {
             case 'dup':
@@ -75,7 +75,7 @@ $json = json_encode($a->fetchAll(PDO::FETCH_ASSOC));
                 });
         }
 
-        for (var i = 0; i < document.getElementsByClassName('numW').length; i++) {
+        for (let i = 0; i < document.getElementsByClassName('numW').length; i++) {
             document.getElementsByClassName('numW')[i].innerText = i + 1;
         }
     }
